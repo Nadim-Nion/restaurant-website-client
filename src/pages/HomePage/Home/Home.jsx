94d8477f7aeb070595eea88dd5d44cf3c363c381
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-26 19:11:50 +0600

    Footer and JSON file added

--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>This is Home Page</h2>
+            <div className='text-center'>
+                <h2 className='uppercase text-3xl text-red-700 font-bold'>Chicken Crisper Combos</h2>
+                <p>Welcome to Chicken Crisper Combos – your go-to spot for mouthwatering burgers, pizzas, and a wide selection of delicious treats crafted to satisfy every craving!</p>
+            </div>
         </div>
     );
 };
