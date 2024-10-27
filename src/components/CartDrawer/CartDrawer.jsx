8678eff2aa26362c4e5eac29e737ef2fc857de3d
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-10-27 22:03:43 +0600

    close button to the sidebar

--- src/components/CartDrawer/CartDrawer.jsx
+++ src/components/CartDrawer/CartDrawer.jsx
@@ -12,11 +12,22 @@ const CartDrawer = () => {
 
             <div className="drawer-side">
                 <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
+
+                <div>
+                    <label
+                        htmlFor="my-drawer-4"
+                        aria-label="Close drawer"
+                        className="btn btn-sm btn-circle absolute right-4">
+                        X
+                    </label>
+
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        {/* Sidebar content here */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     );
